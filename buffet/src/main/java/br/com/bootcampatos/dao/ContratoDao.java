a3467cc71285628b3d0209e4b2b4e90adfc93da1
DevFerraz <104537447+DevFerraz@users.noreply.github.com>
2022-11-09 20:47:28 -0300

    Modelo final

--- buffet/src/main/java/br/com/bootcampatos/dao/ContratoDao.java
+++ buffet/src/main/java/br/com/bootcampatos/dao/ContratoDao.java
@@ -1,10 +1,9 @@
 package br.com.bootcampatos.dao;
 
-import java.util.List;
+import br.com.bootcampatos.model.Contrato;
 
 import javax.persistence.EntityManager;
-
-import br.com.bootcampatos.model.Contrato;
+import java.util.List;
 
 public class ContratoDao extends DAO<Contrato> {
     public ContratoDao(EntityManager em) {
@@ -13,9 +12,9 @@ public class ContratoDao extends DAO<Contrato> {
 
 
     public List<Contrato> findByNome(String nome){
-        return em.createQuery("select u from Contrato u inner join u.cliente cliente where cliente.nome = :nome",
+        return em.createQuery("select u from Contrato u inner join u.cliente cliente where cliente.nome LIKE :nome",
                         Contrato.class)
-                .setParameter("nome", nome)
+                .setParameter("nome", "%" + nome + "%")
                 .getResultList();
     }
     public Contrato findById(Long id){
